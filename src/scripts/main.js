631a81ee9edfad89abf8f47155b6b5fa076fe600
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2020-11-26 01:56:00 -0500

    opening animation, nav/skeleton progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll("#burger"), 0);
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
@@ -13,14 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
-        const $target = document.getElementById(target);
+        const $target = document.getElementById("nav");
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
 
+        if ($target.classList.contains("conditionalClose")) {
+          $target.classList.replace("conditionalClose", "conditionalOpen");
+          $target.classList.add("is-active");
+        } else {
+          $target.classList.replace("conditionalOpen", "conditionalClose");
+          $target.classList.remove("is-active");
+        }
       });
     });
   }
 
 });
+
+//slide nav open
